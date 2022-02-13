24d19b6bbb2394df26346331116e47e4e8e1afa7
burakqull <83167665+burakqull@users.noreply.github.com>
2022-02-14 01:11:19 +0300

    Add files via upload

---
+++ python uzaktan akademi ve spark-ed/egitim-2.py
@@ -0,0 +1,24 @@
+"""
+veri tipleri 2 ye ayrılır 
+
+A-İMMUTABLE(DEĞİŞTİRELEMEZ)
+Aa-numbers
+Ab-strings
+Ac-tuple
+
+B-MUTABLE(DEĞİŞTİRELEBİLİR)
+Ba-list
+Bb-set
+Bc-dictionary
+
+Bb-List
+
+
+
+
+
+"""
+
+
+
+
